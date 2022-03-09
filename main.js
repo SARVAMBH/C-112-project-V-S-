602ab05f9f7a22ac08598ca3f89acd823c969a65
SARVAMBH <83635572+SARVAMBH@users.noreply.github.com>
2022-03-09 13:13:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+prediction1 = "";
+prediction2 = "";
+
+Webcam.set({
+   width: 350,
+   height: 300,
+   image_format: 'png',
+   png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    })
+}
+
+console.log('ml5 version:' , ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Pfgv2uGx3/model.json' , modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak() 
+{
+   
+   var synth = window.speechSynthesis;
+   var speak_data_1 = "The first prediction is " +prediction1;
+   var speak_data_2 = "And the second prediction is" +prediction2;
+   var utterThis = new SpeechSynthesisUtterance (speak_data_1 + speak_data_2);
+   synth.speak(utterThis);
+
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img , gotResult);
+}
+
+function gotResult(error , results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+
+          if(results[0].label == "punch")
+          {
+              document.getElementById("update_gesture").innerHTML = "&#9994;";
+          }
+
+          if(results[0].label == "victory")
+          {
+              document.getElementById("update_gesture").innerHTML = "&#9996;";
+          }
+
+          if(results[0].label == "amazing")
+          {
+              document.getElementById("update_gesture").innerHTML = "&#128076;";
+          }
+
+
+          if(results[1].label == "punch")
+          {
+              document.getElementById("update_gesture2").innerHTML = "&#9994;";
+          }
+
+          if(results[1].label == "victory")
+          {
+              document.getElementById("update_gesture2").innerHTML = "&#9996;";
+          }
+
+          if(results[1].label == "amazing")
+          {
+              document.getElementById("update_gesture2").innerHTML = "&#128076;";
+          }
+
+    }
+}
